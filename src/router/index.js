27ca75f6dbdbe25a91466940fa8eb14d1abf1d94
zhanruihan <Ruihan_zhan_hz@163.com>
2023-01-31 13:53:02 +0800

    delete files

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,14 @@ const login = (r) => require.ensure([], () => r(require('@/page/login')), 'login
 const manage = (r) => require.ensure([], () => r(require('@/page/manage')), 'manage')
 const home = (r) => require.ensure([], () => r(require('@/page/home')), 'home')
 const addShop = (r) => require.ensure([], () => r(require('@/page/addShop')), 'addShop')
-const addGoods = (r) => require.ensure([], () => r(require('@/page/addGoods')), 'addGoods')
-const userList = (r) => require.ensure([], () => r(require('@/page/userList')), 'userList')
-const shopList = (r) => require.ensure([], () => r(require('@/page/shopList')), 'shopList')
+
 const foodList = (r) => require.ensure([], () => r(require('@/page/foodList')), 'foodList')
-const orderList = (r) => require.ensure([], () => r(require('@/page/orderList')), 'orderList')
-const adminList = (r) => require.ensure([], () => r(require('@/page/adminList')), 'adminList')
+
 const visitor = (r) => require.ensure([], () => r(require('@/page/visitor')), 'visitor')
 const svgExample = (r) => require.ensure([], () => r(require('@/page/svgExample')), 'svgExample')
-const newMember = (r) => require.ensure([], () => r(require('@/page/newMember')), 'newMember')
+
 const uploadImg = (r) => require.ensure([], () => r(require('@/page/uploadImg')), 'uploadImg')
 const vueEdit = (r) => require.ensure([], () => r(require('@/page/vueEdit')), 'vueEdit')
-const adminSet = (r) => require.ensure([], () => r(require('@/page/adminSet')), 'adminSet')
-const sendMessage = (r) => require.ensure([], () => r(require('@/page/sendMessage')), 'sendMessage')
-const explain = (r) => require.ensure([], () => r(require('@/page/explain')), 'explain')
 
 const routes = [
   {
@@ -42,36 +36,13 @@ const routes = [
         component: addShop,
         meta: ['添加数据', '添加商铺'],
       },
-      {
-        path: '/addGoods',
-        component: addGoods,
-        meta: ['添加数据', '添加商品'],
-      },
-      {
-        path: '/userList',
-        component: userList,
-        meta: ['数据管理', '用户列表'],
-      },
-      {
-        path: '/shopList',
-        component: shopList,
-        meta: ['数据管理', '商家列表'],
-      },
+
       {
         path: '/foodList',
         component: foodList,
         meta: ['数据管理', '食品列表'],
       },
-      {
-        path: '/orderList',
-        component: orderList,
-        meta: ['数据管理', '订单列表'],
-      },
-      {
-        path: '/adminList',
-        component: adminList,
-        meta: ['数据管理', '管理员列表'],
-      },
+
       {
         path: '/visitor',
         component: visitor,
@@ -82,11 +53,7 @@ const routes = [
         component: svgExample,
         meta: ['图表', 'svgExample'],
       },
-      {
-        path: '/newMember',
-        component: newMember,
-        meta: ['图表', '用户数据'],
-      },
+
       {
         path: '/uploadImg',
         component: uploadImg,
@@ -96,21 +63,6 @@ const routes = [
         path: '/vueEdit',
         component: vueEdit,
         meta: ['编辑', '文本编辑'],
-      },
-      {
-        path: '/adminSet',
-        component: adminSet,
-        meta: ['设置', '管理员设置'],
-      },
-      {
-        path: '/sendMessage',
-        component: sendMessage,
-        meta: ['设置', '发送通知'],
-      },
-      {
-        path: '/explain',
-        component: explain,
-        meta: ['说明', '说明'],
       },
     ],
   },
